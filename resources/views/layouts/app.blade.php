6d6655ec5ace735ca152200aff195da7220de0dd
Tonismar <tonismar@gmail.com>
2016-03-22 22:11:56 -0300

    Abra-te cesamo!

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Laravel Quickstart - Basic</title>
+	</head>
+	
+	<body>
+		<div class="container">
+			<nav class="navbar navbar-default">
+			</nav>
+		</div>
+		
+		@yield('content')
+	</body>
+</html>
